02f1555eee9ee29b7e130eae2bf39f27c6891571
vanshaj <vanshaj.gangwar@bonamisoftware.com>
2025-06-25 15:47:31 +0530

    api

--- handling_api/api.py
+++ handling_api/api.py
@@ -5,7 +5,7 @@ def fetch_random_user():
     url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
     response = requests.get(url)
     data = response.json()
-    print(data)
+    print(f"data: {data}")
 
     if data["success"] and "data" in data:
         user_data = data["data"]
@@ -13,3 +13,17 @@ def fetch_random_user():
         country = user_data["location"]["country"]
 
         return username, country
+    else:
+        raise Exception("Failed to fetch user data!")
+
+
+def main():
+    try:
+        username, country = fetch_random_user()
+        print(f"username = {username} and country = {country}")
+    except Exception as e:
+        print(str(e))
+
+
+if __name__ == "__main__":
+    main()
